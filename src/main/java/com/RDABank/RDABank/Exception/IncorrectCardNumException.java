eedc6d7e9b2ebc8a1ac4761cd319a0767189f2ae
HP <divyashreeharitha@gmail.com>
2024-03-20 20:22:37 +0530

    Exception name 2 commit

---
+++ src/main/java/com/RDABank/RDABank/Exception/IncorrectCardNumException.java
@@ -0,0 +1,7 @@
+package com.RDABank.RDABank.Exception;
+
+public class IncorrectCardNumException extends RuntimeException{
+    public IncorrectCardNumException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/RDABank/RDABank/Exception/IncorrectCardNumberException.java
+++
@@ -1,7 +0,0 @@
-package com.RDABank.RDABank.Exception;
-
-public class IncorrectCardNumberException extends RuntimeException{
-    public IncorrectCardNumberException(String message){
-        super(message);
-    }
-}
